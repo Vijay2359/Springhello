fba60126057738f0e69c64c1f93bfa335888d269
Vijay2359 <vijaysinh.gaikwad@cognologix.com>
2025-02-13 15:51:17 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/hello_world/HelloWorldController.java
+++ src/main/java/com/example/hello_world/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World! Vijay here";
+        return "Hello World! Testing code changes";
     }
 }
