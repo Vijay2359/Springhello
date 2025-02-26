01d0f417d148952cf4c06cc9219ab34a5dd86c2e
Vijay2359 <vijaysinh.gaikwad@cognologix.com>
2025-02-26 13:14:44 +0530

    Update App.java

--- src/main/java/com/example/hello_world/App.java
+++ src/main/java/com/example/hello_world/App.java
@@ -1,13 +1,11 @@
-package com.example;
+package com.example; // Ensure this matches your project structure
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App.java{
+public class App {  // Class name should not include .java
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 }
-
-
