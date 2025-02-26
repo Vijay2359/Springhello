de673733f09c19f77016d040e517734eb1982b31
Vijay2359 <vijaysinh.gaikwad@cognologix.com>
2025-02-26 13:09:43 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/hello_world/HelloWorldController.java
+++ src/main/java/com/example/hello_world/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
-public class HelloController {
+public class App {
 
     @Value("${APP_COLOR:default}")
     private String appColor;
